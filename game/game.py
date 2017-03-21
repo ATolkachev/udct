cde735957fb313262b445b38b8888666725728b8
Virtim <timvir@gmail.com>
2017-03-21 12:09:05 -0700

    Fix for level_of_dif
    Also answer for 1 in comments on top

--- game/game.py
+++ game/game.py
@@ -1,3 +1,7 @@
+#How can you store your data (your questions) to reduce redundancy?
+# make a list of strings and use indexes. level 1 use question[0], level 3 use question[0..2]
+# question = ['If you multiply 2 by 2 correct answer is going to be: __1__.', 'If you then multiply it by 3 it became __2__.', 'And if you add 9 it'll became __3__.']
+
 #questions
 question1 = 'If you multiply 2 by 2 correct answer is going to be: __1__.'
 question2 = 'If you multiply 2 by 2 correct answer is going to be: __1__. If you then multiply it by 3 it became __2__.'
@@ -15,23 +19,15 @@ level = 0
 #empty variable with final answer
 final_answer = []
 # Select a level
-#Why it's not working with the first try and doesn't return errors from else?
+#Why it's not working with the first try and doesn't return errors from else? --fixed
+#Try and Except statements are unnecessary here. -- fixed with accepting string and comparing input with strings
 def level_of_dif():
-    level = int(input("How dificult is this quiz supposed to be? \n 1. Super ease \n 2. Harder \n 3. Very hard \n"))
-    try:
-        if level == 1 or level == 2 or level == 3:
-            return level
-        else:
-            print 'Sorry, but you can only choose between 1 to 3 here'
-            level_of_dif()
-    except NameError:
-        print 'Sorry, but you can only use integers here'
-        level_of_dif()
-
-#split string with question to a list
-#question1 = question1.split()
-#question2 = question2.split()
-#question3 = question3.split()
+    level = raw_input("How dificult is this quiz supposed to be? \n 1. Super ease \n 2. Harder \n 3. Very hard \n")
+    if level == '1' or level == '2' or level == '3':
+        return level
+    else:
+        print 'Sorry, but you can only choose between 1 to 3 here. PLease use numbers only'
+        return level_of_dif()
 
 #looking for task in list_of_questions
 
@@ -54,15 +50,16 @@ def check_answers(level):
     answer_index = 0
     print "Current answer_index is ", answer_index
     while answer_index < level:
-        print "Answer index is: ", answer_index, "Which is <= ", level
-        user_input = raw_input("Type in an answer: ")
-        if user_input == answers[answer_index]:
-            answer_index += 1
-            print "new answer index is: ", answer_index
-            print 'Correct!'
-        else:
-            print 'Try to use calculator and type an answer again: '
-            check_answers(level)
+        if answer_index < len(answers):
+            print "Answer index is: ", answer_index, "Which is <= ", level
+            user_input = raw_input("Type in an answer: ")
+            if user_input == answers[answer_index]:
+                answer_index += 1
+                print "new answer index is: ", answer_index
+                print 'Correct!'
+            else:
+                print 'Try to use calculator and type an answer again: '
+                check_answers(level)
     return "All answers are correct!"
 
 def play_game(question_string, loq):
@@ -79,4 +76,4 @@ def play_game(question_string, loq):
     return replaced
 
 #print check_answers(level_of_dif())
-print play_game(assign_lod_to_quest(level_of_dif), list_of_questions)
+#print play_game(assign_lod_to_quest(level_of_dif), list_of_questions)
