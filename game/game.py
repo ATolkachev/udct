d0fc453c89fac99aed1dc2acccc43e081984464c
Virtim <tminnakhmetov@yahoo.com>
2017-03-21 17:04:52 -0700

    After yapf

--- game/game.py
+++ game/game.py
@@ -11,31 +11,38 @@ question3 = "If you multiply 2 by 2 correct answer is going to be: __1__. If you
 answers = ['4', '12', '21']
 
 #Array to search through question
-list_of_questions =['__1__', '__2__', '__3__']
+list_of_questions = ['__1__', '__2__', '__3__']
 
 #variable with level
 level = 0
 
 #empty variable with final answer
 final_answer = []
+
+
 # Select a level
 #Why it's not working with the first try and doesn't return errors from else? --fixed
 #Try and Except statements are unnecessary here. -- fixed with accepting string and comparing input with strings
 def level_of_dif():
-    level = raw_input("How dificult is this quiz supposed to be? \n 1. Super ease \n 2. Harder \n 3. Very hard \n")
+    level = raw_input(
+        "How dificult is this quiz supposed to be? \n 1. Super ease \n 2. Harder \n 3. Very hard \n"
+    )
     if level == '1' or level == '2' or level == '3':
         return level
     else:
         print 'Sorry, but you can only choose between 1 to 3 here. PLease use numbers only'
         return level_of_dif()
 
+
 #looking for task in list_of_questions
+
 
 def task_in_loq(task, loq):
     for element in loq:
         if element in task:
             return element
     return None
+
 
 #Returns string based on level of dificulty
 def assign_lod_to_quest(lod):
@@ -45,6 +52,7 @@ def assign_lod_to_quest(lod):
         return question2
     else:
         return question3
+
 
 def check_answers(level):
     answer_index = 0
@@ -62,6 +70,7 @@ def check_answers(level):
                 check_answers(level)
     return "All answers are correct!"
 
+
 def play_game(question_string, loq):
     replaced = []
     question_string = question_string.split()
@@ -74,6 +83,7 @@ def play_game(question_string, loq):
             replaced.append(word)
     replaced = ' '.join(replaced)
     return replaced
+
 
 #print check_answers(level_of_dif())
 #print play_game(assign_lod_to_quest(level_of_dif), list_of_questions)
