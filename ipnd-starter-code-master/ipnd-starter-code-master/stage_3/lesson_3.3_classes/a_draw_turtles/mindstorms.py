7586a802001d738fe1784c9bbdc8bf21b9d33ef3
Virtim <tminnakhmetov@yahoo.com>
2017-03-22 20:36:15 -0700

    Turtle is done!

--- ipnd-starter-code-master/ipnd-starter-code-master/stage_3/lesson_3.3_classes/a_draw_turtles/mindstorms.py
+++ ipnd-starter-code-master/ipnd-starter-code-master/stage_3/lesson_3.3_classes/a_draw_turtles/mindstorms.py
@@ -10,3 +10,60 @@
 import turtle
 
 # Your code here.
+
+def draw_square(some_turtle):
+    for i in range(1,5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+
+def draw_figures():
+
+    window = turtle.Screen()
+    window.bgcolor("green")
+
+#    bernie = turtle.Turtle()
+#    bernie.forward(200)
+#    bernie.right(90)
+#    bernie.forward(200)
+#    bernie.right(135)
+#    bernie.forward(282.842712475)
+
+
+    tim = turtle.Turtle()
+    tim.speed(1)
+    tim.color("black")
+    tim.forward(100)
+    tim.backward(50)
+    tim.right(90)
+    tim.forward(100)
+
+    tim.up()
+
+    tim.setpos(150, -100)
+    tim.down()
+    tim.left(180)
+    tim.forward(100)
+    tim.right(135)
+    tim.forward(80)
+    tim.left(90)
+    tim.forward(80)
+    tim.right(135)
+    tim.forward(100)
+
+
+    brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color('red')
+    brad.speed(10)
+    for i in range(1,36):
+        draw_square(brad)
+        brad.right(10)
+
+    angie = turtle.Turtle()
+    angie.circle(100)
+    angie.shape("arrow")
+    angie.color("blue")
+
+    window.exitonclick()
+
+draw_figures()
