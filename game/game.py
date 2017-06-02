e1e6fac6fc217987538cf174cfa5556f33b87830
Alexander Tolkachev <atolkachev@ru-06478038.ru.corp.acronis.com>
2017-06-02 21:54:53 +0300

    Make more complicated!

--- game/game.py
+++ game/game.py
@@ -1,37 +1,38 @@
+#!/bin/python
+
 #How can you store your data (your questions) to reduce redundancy?
 # make a list of strings and use indexes. level 1 use question[0], level 3 use question[0..2]
-final_answer = "If you multiply 2 by 2 correct answer is going to be: 4. If you then multiply it by 3 it became 12. And if you add 9 it'll became 21."
+final_answer = "If you multiply 2 by 2 correct answer is going to be: 4. If you then multiply it by 3 it became 12. And if you add 9 it'll became 21. And if you add 1 it'll became 22."
 
 #questions
 question1 = 'If you multiply 2 by 2 correct answer is going to be: __1__.'
 question2 = 'If you multiply 2 by 2 correct answer is going to be: __1__. If you then multiply it by 3 it became __2__.'
 question3 = "If you multiply 2 by 2 correct answer is going to be: __1__. If you then multiply it by 3 it became __2__. And if you add 9 it'll became __3__."
+question4 = "If you multiply 2 by 2 correct answer is going to be: __1__. If you then multiply it by 3 it became __2__. And if you add 9 it'll became __3__. And if you add 1 it'll became __4__."
+
+questions = ["If you multiply 2 by 2 correct answer is going to be: __1__.","If you then multiply it by 3 it became __2__.","And if you add 9 it'll became __3__.","And if you add 1 it'll became __4__."]
 
 #list with answers
-answers = ['4', '12', '21']
+answers = [4, 12, 21, 22]
 
 #how many tries user have before we realise he is not good enough
 tries = 6
 
 #Array to search through question
-list_of_blanks = ['__1__', '__2__', '__3__']  #Find list_of_blanks
+list_of_blanks = ['__1__', '__2__', '__3__','__4__']  #Find list_of_blanks
 
 # docstrings <----
 # Select a level
 # Why it's not working with the first try and doesn't return errors from else? --fixed
 # Try and Except statements are unnecessary here. -- fixed with accepting string and comparing input with strings
 def level_of_dif():
-    level = raw_input(
-        "How dificult is this quiz supposed to be? \n 1. Super ease \n 2. Harder \n 3. Very hard \n"
-    )
-    if level == '1':
-        return question1, answers[0].split()
-    elif level == '2':
-        return question2, answers[:2]
-    elif level == '3':
-        return question3, answers
+    level = eval(raw_input(
+        "How dificult is this quiz supposed to be? \n 1. Super ease \n 2. Harder \n 3. Very hard \n 4. Hardcore \n"
+    ))
+    if level <= 4:
+        return questions[:level], answers[:level]
     else:
-        print 'Sorry, but you can only choose between 1 to 3 here. PLease use numbers only'
+        print 'Sorry, but you can only choose between 1 to 4 here. PLease use numbers only'
         return level_of_dif()
 
 
@@ -51,11 +52,11 @@ def play_game(question_string, lob):
     exact_questions = [
         'What the answer for the first blank? ',
         'What the answer for the second blank? ',
-        'What the answer for the third blank? '
+        'What the answer for the third blank? ',
+        'What the answer for the fourth blank? '
     ]
     exact_questions_index = 0
     print "Here is the question: ", question, "Please feel the blanks"
-    question_string = question_string.split()
     for word in question_string:
         replacement = task_in_lob(word, lob)
         if replacement != None:
@@ -71,7 +72,6 @@ def play_game(question_string, lob):
     elif tries > 0:
         tries -= 1
         print "You made a mistake. Try to use calculator. You have only tries", tries, "left"
-        question_string = ' '.join(question_string)
         play_game(question_string, lob)
     else:
         print "Too many mistakes. Goodbye"
